374fa17ec93c958ef08308c1d43b99c0c0695fc3
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-08 18:48:44 +0700

    changed

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        Scanner sc = new Scanner()
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello : " + name);
     }
 }
---
+++ src/ReadNumByChar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReadNumByChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(read(i));
+        }
+    }
+
+    static String read(int number) {
+
+    }
+}
+
